ac0f8aec7fbae248c52fdaca00b59c0ad22829a6
wortel13 <lowiss_89@Hotmail.com>
2020-03-23 22:11:47 +0700

    stirng sm switch kelar

---
+++ PR-Hacktiv8/Week 2/String/breaking.js
@@ -0,0 +1,19 @@
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word.substring(0, 3);
+var secondWord = word.substring(4, 14);
+var thirdWord = word.substring(15, 17); // do your own!
+var fourthWord = word.substring(18, 20); // do your own!
+var fifthWord = word.substring(21, 25); // do your own!
+
+
+var firstWordLength = exampleFirstWord.length;
+var secondWordLength = secondWord.length;
+var thirdwordLength = thirdWord.length;
+var fourthWordLength = fourthWord.length;
+var fifthWordLength = fifthWord.length;
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord + ', with length: ' + thirdwordLength);
+console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWordLength);
--- PR-Hacktiv8/Week 2/switch case/switchh.js
+++ PR-Hacktiv8/Week 2/switch case/switchh.js
@@ -1,19 +1,54 @@
-var tanggal = '1'; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
-var bulan = "Januari" ; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
-var tahun = '1990' ; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+var tanggal = 1; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = "Desember" ; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = '2000' ; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+if (tanggal > 31 || tanggal < 1) {
+	console.log("tanggal salah");
+} else if (tahun < 1900 || tahun > 2200) {
+	console.log("tahun salah");
+} else {
 
 switch (bulan) {
 	case "Januari":
 		console.log(tanggal + " " + bulan + " " + tahun);
 		break;
-	case "Januari":
+	case "Februari":
 		console.log(tanggal + " " + bulan + " " + tahun);
 		break;
-	case "Januari":
+	case "Maret":
 		console.log(tanggal + " " + bulan + " " + tahun);
 		break;
-	
+	case "April":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "Mei":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "Juni":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "Juli":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "Agustus":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "September":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "Oktober":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "November":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "Desember":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+
 	default:
 		console.log('salah');
 		break;
+}
+
 }
