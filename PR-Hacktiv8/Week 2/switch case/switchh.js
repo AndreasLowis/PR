02df856af14fe2a281fe2669c54a8e2e8246ed65
Andreas Lowis <61585345+AndreasLowis@users.noreply.github.com>
2020-03-23 20:22:15 +0700

    Add files via upload

---
+++ PR-Hacktiv8/Week 2/switch case/switchh.js
@@ -0,0 +1,19 @@
+var tanggal = '1'; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = "Januari" ; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = '1990' ; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+switch (bulan) {
+	case "Januari":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "Januari":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	case "Januari":
+		console.log(tanggal + " " + bulan + " " + tahun);
+		break;
+	
+	default:
+		console.log('salah');
+		break;
+}
