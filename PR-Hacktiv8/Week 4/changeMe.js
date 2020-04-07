b93757c6896c1f67d6ab0572231fd374e1b3f508
wortel13 <lowiss_89@Hotmail.com>
2020-04-07 11:47:28 +0700

    change me age kelar

--- PR-Hacktiv8/Week 4/changeMe.js
+++ PR-Hacktiv8/Week 4/changeMe.js
@@ -2,19 +2,16 @@ function changeMe(arr) {
   // you can only write your code here!
   var array = {};
   
-  var nol = 1;
 
   for( i = 0; i < arr.length; i++ ) {
     var obj = {}
-
-
     obj.firstName = arr[i][0];
     obj.lastName = arr[i][1];
     obj.gender = arr[i][2];
-    obj.age = arr[i][3];
+    if( arr[i][3] == undefined ){
+      obj.age = 'Invalid Birth Year';
+    } else obj.age = Math.abs(arr[i][3] - 2020);
     
-    // console.log(obj);
-
     array[i+1 + ". " + arr[i][0] + " " + arr[i][1]] = obj;
   }
   return array;
