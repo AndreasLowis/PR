faea9f473579ec001f3481dfc7960f3fbd3faf9d
wortel13 <lowiss_89@Hotmail.com>
2020-04-18 22:19:33 +0700

    Regex

---
+++ PR-Hacktiv8/Week 5/cariPelaku.js
@@ -0,0 +1,12 @@
+function cariPelaku(str) {
+  // you can only write your code here!
+  console.log(str.match(/abc/g))
+  return str.match(/abc/g).length
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
